8e0b6371db460504113704d39ff014bce30fbbf8
m-i-chan <michan1910@gmail.com>
2026-04-22 16:20:16 -0400

    printer functions

--- scripts/main.py
+++ scripts/main.py
@@ -1,14 +1,14 @@
 """
 main.py
 
-Eventual entry point for tic-tac-toe app.
+Entry point for tic-tac-toe.
 """
 
 import sys
 import time
 from printrun.printcore import printcore
 
-import scripts.tictactoe as tictactoe
+import tictactoe as ttt
 
 if __name__ == "__main__":
     if sys.version_info.major != 3 and sys.version_info.minor != 12:
@@ -17,12 +17,13 @@ if __name__ == "__main__":
     baud = input("Enter baud rate (default 115200): ")
     if baud == "":
         baud = 115200
-    offset = {"x": 0,"y": 0, "z": 0}
+    offset = {"x": 0,"y": 0, "z": 12.5}
     response = input("Pen offset? [y/n]")
     if response.lower() == "y":
         for key in offset.keys():
             offset[key] = input(f"{key} offset in mm: ")
     p = printcore(port,baud)
-    game = tictactoe.Printer(p,offset["x"],offset["y"],offset["z"])
-    game.setup()
+    setup = ttt.Printer(p,offset["x"],offset["y"],offset["z"])
+    setup.home()
+    setup.load_pen()
     p.disconnect()
