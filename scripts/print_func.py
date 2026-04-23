ecb146c352e1b964ceb57f269c16ebae7109c96c
m-i-chan <michan1910@gmail.com>
2026-04-22 20:21:49 -0400

    draw_x and draw_o functions

--- scripts/print_func.py
+++ scripts/print_func.py
@@ -1,4 +1,5 @@
 from printrun.printcore import printcore
+import time
 
 class Printer():
     # Pen offset from extruder in mm.
@@ -14,27 +15,52 @@ class Printer():
         self.pen_z = z
 
     def home(self):
+        self.p.send('G0 F6000;') # Increase move rate.
         self.p.send('G28 ;') # Home printer
     
     def center(self):
-        self.p.send(f'G0 X{110-self.pen_x} Y{110-self.pen_y} Z125 F3000') # Centers pen on Ender 3 with default bed size of 220x220mm.
+        self.p.send(f'G0 X{110-self.pen_x} Y{110-self.pen_y} Z50 F6000;') # Centers pen on Ender 3 with default bed size of 220x220mm.
     
     def load_pen(self):
-        self.p.send(f'G0 X{110-self.pen_x} Y{200-self.pen_y} Z25.4') # Brings print head to front of printer to enable loading of pen.
+        self.p.send(f'G0 X{110-self.pen_x} Y{self.pen_y} Z25.4;') # Brings print head to front of printer to enable loading of pen.
         input("Press enter when pen is loaded.") # Awaits user confirmation that pen is loaded.
-        self.p.send(f'G0 Z{self.pen_z}') # Lowers print head to meet desired pen_z offset and ensure pen is at correct height.
+        self.p.send(f'G0 Z{self.pen_z} F500;') # Lowers print head to meet desired pen_z offset and ensure pen is at correct height.
         self.center()
+
+    def go_to(self,x,y):
+        self.p.send(f'G0 X{x-self.pen_x} Y{y-self.pen_y};')
     
     def draw_line(self, start_x, start_y, end_x, end_y):
-        self.p.send(f'G0 Z{self.LIFT}')
-        self.p.send(f'G0 X{start_x} Y{start_y}')
-        self.p.send(f'G0 Z{self.pen_z}')
-        self.p.send(f'G0 X{end_x} Y{end_y}')
-        self.p.send(f'G0 Z){self.LIFT}')
+        self.p.send(f'G0 Z{self.LIFT};')
+        self.p.send(f'G0 X{start_x-self.pen_x} Y{start_y-self.pen_y};')
+        self.p.send(f'G0 Z{self.pen_z};')
+        self.p.send(f'G0 X{end_x-self.pen_x} Y{end_y-self.pen_y};')
+        self.p.send(f'G0 Z{self.LIFT};')
+        time.sleep(1)
     
-    def draw_circle(self, start_x, start_y, radius):
-        self.p.send(f'G0 Z{self.LIFT}')
-        self.p.send(f'G0 X{start_x} Y{start_y}')
-        self.p.send(f'G0 Z{self.pen_z}')
-        self.p.send(f'G2 R{radius}')
-        self.p.send(f'G0 Z{self.LIFT}')
+    def draw_circle(self, start_x, start_y, i_offset, j_offset):
+        self.p.send(f'G0 Z{self.LIFT};')
+        self.p.send(f'G0 X{start_x-self.pen_x} Y{start_y-self.pen_y};')
+        self.p.send(f'G0 Z{self.pen_z};')
+        self.p.send(f'G2 I{i_offset} J{j_offset};')
+        self.p.send(f'G0 Z{self.LIFT};')
+        time.sleep(1)
+    
+    def draw_grid(self):
+        self.draw_line(50,130,170,130)
+        self.draw_line(50,90,170,90)
+        self.draw_line(90,170,90,50)
+        self.draw_line(130,170,130,50)
+    
+    def draw_o(self,grid_x=0,grid_y=0):
+        offset_x = 70 + 40 * grid_x
+        offset_y = 168 - 40 * grid_y
+        self.draw_circle(offset_x,offset_y,0,-18)
+    
+    def draw_x(self,grid_x=0,grid_y=0):
+        offset_x = 52 + 40 * grid_x
+        offset_y = 168 - 40* grid_y
+        self.draw_line(offset_x,offset_y,offset_x + 36, offset_y - 36)
+        self.draw_line(offset_x,offset_y-36,offset_x + 36, offset_y)
+
+        
