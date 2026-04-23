3880e05caf8dc231cbf48e6fa8cf20955915874b
m-i-chan <michan1910@gmail.com>
2026-04-22 20:22:02 -0400

    quick and dirty game

--- scripts/tictactoe.py
+++ scripts/tictactoe.py
@@ -1,6 +1,5 @@
 class Board():
     # -1 for player 1, 0 for empty, 1 for player 2
-
     def __init__(self):
         self.board = [[0, 0, 0],
                       [0, 0, 0],
@@ -8,28 +7,27 @@ class Board():
 
     def mark_board(self,player,row,col):
         if self.board[row][col] != 0:
-            return False
+            raise ValueError("Space occupied")
         self.board[row][col] = player
-        return True
 
     def get_Winner(self):
         # Returns -1 if player 1 won, 0 if no winner, 1 for player 2
         for row in self.board: # Check rows
             sum = 0
-            for col in self.board:
-                sum += self.board[row][col]
+            for col in row:
+                sum += col
             if sum == 3:
                 return 1
-            elif sum == -3:
+            if sum == -3:
                 return -1
 
-        for col in self.board: # Check columns
+        for x in range(3): # Check columns
             sum = 0
-            for row in self.board:
-                sum += self.board[row][col]
+            for y in range(3):
+                sum += self.board[y][x]
             if sum == 3:
                 return 1
-            elif sum == -3:
+            if sum == -3:
                 return -1
 
         # Check diagonals
@@ -38,13 +36,64 @@ class Board():
         sum += self.board[2][2]
         if sum == 3:
             return 1
-        elif sum == -3:
+        if sum == -3:
             return -1
         sum = self.board[1][1]
         sum += self.board[0][2]
         sum += self.board[2][0]
         if sum == 3:
             return 1
-        elif sum == -3:
+        if sum == -3:
             return -1
         return 0
+    
+    def clear_board(self):
+        self.board = [[0,0,0],
+                      [0,0,0],
+                      [0,0,0]]
+    
+    def is_full(self):
+        for row in self.board:
+            for col in row:
+                if col == 0:
+                    return False
+        return True
+    
+    def __str__(self):
+        out = ""
+        for row in self.board:
+            out += str(row) + "\n"
+        return out
+
+class Game():
+    def __init__(self):
+        self.board = Board()
+        self.player = -1
+    
+    def next_player(self):
+        self.player *= -1
+    
+    def turn(self, player, row, col):
+        self.board.mark_board(player, row, col)
+        self.next_player()
+    
+    def run_game(self):
+        game_on = True
+        while game_on:
+            print(f'Player {"1" if self.player == -1 else "2"}, enter your move.')
+            response = input().split(",")
+            try:
+                self.turn(self.player,int(response[0]),int(response[1]))        
+            except ValueError:
+                print("Space occupied.")
+            winner = self.board.get_Winner()
+            if winner == -1:
+                game_on = not game_on
+                print('Player 1 wins!')
+            elif winner == 1:
+                game_on = not game_on
+                print('Player 2 wins!')
+            if self.board.is_full():
+                game_on = not game_on
+                print("Tie game")
+            print(self.board)
