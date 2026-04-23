ed950d256cbf9551d253f869e9f303e7c2dde428
m-i-chan <michan1910@gmail.com>
2026-04-22 21:37:31 -0400

    added comments, minor function changes

--- scripts/print_func.py
+++ scripts/print_func.py
@@ -1,3 +1,9 @@
+"""
+print_func.py
+
+Contains printer manipulation functions
+"""
+
 from printrun.printcore import printcore
 import time
 
@@ -6,7 +12,9 @@ class Printer():
     pen_x = 0
     pen_y = 0
     pen_z = 12.5
-    LIFT = pen_z + 10 # Height to lift pen while drawing
+
+
+    lift_z = pen_z + 5 # Height to lift pen after drawing.
 
     def __init__(self,p:printcore,x:float=0,y:float=0,z:float=0):
         self.p = p
@@ -30,34 +38,41 @@ class Printer():
     def go_to(self,x,y):
         self.p.send(f'G0 X{x-self.pen_x} Y{y-self.pen_y};')
     
+    def go_to(self,x,y,z):
+        self.p.send(f'G0 X{x-self.penx} Y{y-self.pen_y} Z{z-self.pen_z}')
+
     def draw_line(self, start_x, start_y, end_x, end_y):
-        self.p.send(f'G0 Z{self.LIFT};')
+        self.p.send(f'G0 Z{self.lift_z};')
         self.p.send(f'G0 X{start_x-self.pen_x} Y{start_y-self.pen_y};')
         self.p.send(f'G0 Z{self.pen_z};')
         self.p.send(f'G0 X{end_x-self.pen_x} Y{end_y-self.pen_y};')
-        self.p.send(f'G0 Z{self.LIFT};')
+        self.p.send(f'G0 Z{self.lift_z};')
         time.sleep(1)
     
     def draw_circle(self, start_x, start_y, i_offset, j_offset):
-        self.p.send(f'G0 Z{self.LIFT};')
+        self.p.send(f'G0 Z{self.lift_z};')
         self.p.send(f'G0 X{start_x-self.pen_x} Y{start_y-self.pen_y};')
         self.p.send(f'G0 Z{self.pen_z};')
         self.p.send(f'G2 I{i_offset} J{j_offset};')
-        self.p.send(f'G0 Z{self.LIFT};')
+        self.p.send(f'G0 Z{self.lift_z};')
         time.sleep(1)
     
     def draw_grid(self):
+        # Draws tic tac toe grid
+        # Currently hard-coded for Ender 3 center.
         self.draw_line(50,130,170,130)
         self.draw_line(50,90,170,90)
         self.draw_line(90,170,90,50)
         self.draw_line(130,170,130,50)
     
     def draw_o(self,grid_x=0,grid_y=0):
+        # Draws an o in specified square of above grid. Grid coordinates are (0,0) for top left square and (2,2) for bottom right.
         offset_x = 70 + 40 * grid_x
         offset_y = 168 - 40 * grid_y
         self.draw_circle(offset_x,offset_y,0,-18)
     
     def draw_x(self,grid_x=0,grid_y=0):
+        #  Draws an x in specified square of above grid. Grid coordinates are (0,0) for top left square and (2,2) for bottom right.
         offset_x = 52 + 40 * grid_x
         offset_y = 168 - 40* grid_y
         self.draw_line(offset_x,offset_y,offset_x + 36, offset_y - 36)
