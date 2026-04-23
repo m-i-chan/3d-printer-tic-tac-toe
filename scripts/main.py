40eff1ea9b22cf6d13e5ff53f56be9c1dbb17ce6
m-i-chan <michan1910@gmail.com>
2026-04-22 22:40:05 -0400

    moved control to app.py

--- scripts/main.py
+++ scripts/main.py
@@ -6,27 +6,13 @@ Entry point for tic-tac-toe.
 
 import sys
 import time
-from printrun.printcore import printcore
+import tkinter as tk
 
-import print_func as pf
-
-def setup():
-    printer = pf.Printer(p,offset["x"],offset["y"],offset["z"])
-    printer.home()
-    printer.load_pen()
+import app
 
 if __name__ == "__main__":
     if sys.version_info.major != 3 and sys.version_info.minor != 12:
         raise Exception("Python 3.12 is required.")
-    port = input("Enter device port: ")
-    baud = input("Enter baud rate (default 115200): ")
-    if baud == "":
-        baud = 115200
-    offset = {"x": 0,"y": 0, "z": 12.5}
-    response = input("Pen offset? [y/n]")
-    if response.lower() == "y":
-        for key in offset.keys():
-            offset[key] = input(f"{key} offset in mm: ")
-    p = printcore(port,baud)
-    setup()
-    p.disconnect()
+    root = tk.Tk()
+    game = app.TicTacToe_App(root)
+    root.mainloop()
